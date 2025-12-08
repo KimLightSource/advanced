5b0e129efe2b82b7b98cdba3c8e5217526a657e6
gwkim <qkek875@gmail.com>
2025-12-08 16:24:21 +0900

    test

--- src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class HelloTraceV2 {
-
+//
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX= "<X-";
